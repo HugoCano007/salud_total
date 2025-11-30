06012aa288b6b766165c19e729136e66755d6d12
Derek Argüelles <arguellesderek3@gmail.com>
2025-11-29 23:47:26 -0500

    Cambio en la etiqueta title del archivo 'login.php'

--- public/login.php
+++ public/login.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Login | Salud Total </title>
 </head>
 <body>
 <div class="container">
